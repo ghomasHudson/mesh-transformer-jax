dfecc95aa05eb49be303651851b6d22f954d0dde
Thomas Hudson <07thomas.hudson@gmail.com>
2022-09-22 19:33:14 +0100

    Add hf login to reqs

--- setup_reqs.sh
+++ setup_reqs.sh
@@ -2,6 +2,7 @@
 
 virtualenv env
 source env/bin/activate
-pip install -r requirements.txt
-pip install --upgrade pip
-pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+#pip install -r requirements.txt
+#pip install --upgrade pip
+#pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+huggingface-cli login
