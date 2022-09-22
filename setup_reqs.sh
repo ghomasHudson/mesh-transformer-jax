d38d508c13e7a40cd68067ecf284830e398c0914
Thomas Hudson <07thomas.hudson@gmail.com>
2022-09-22 15:23:31 +0100

    Add setup script

---
+++ setup_reqs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+virtualenv env
+source env/bin/activate
+pip install -r requirements.txt
+pip install --upgrade pip
+pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
