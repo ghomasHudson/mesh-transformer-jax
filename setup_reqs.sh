1081e440648c454f8e42071992bf97d01457f8b0
Thomas Hudson <07thomas.hudson@gmail.com>
2022-09-28 14:03:13 +0100

    Add wandb login

--- setup_reqs.sh
+++ setup_reqs.sh
@@ -6,3 +6,4 @@ pip install -r requirements.txt
 pip install --upgrade pip
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 huggingface-cli login
+wandb login
